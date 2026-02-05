f985711eccc1112a1a2be0cb5b79ab08cace7b29
DivineUX23 <divineigbinoba23@gmail.com>
2026-02-05 23:27:28 +0100

    Init, and dep completed

--- programs/anchor-amm-starter-q1-26/src/lib.rs
+++ programs/anchor-amm-starter-q1-26/src/lib.rs
@@ -11,10 +11,11 @@ declare_id!("3k6pmDkLLF7FBANTs1ddTCGpsxvDjx8UBc5tUryisKTG");
 pub mod anchor_amm_starter_q1_26 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, 
+        seed: u64, 
+        fee: u16, 
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
         ctx.accounts.init(seed, fee, authority, ctx.bumps)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
